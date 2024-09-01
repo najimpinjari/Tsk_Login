b229b5a13d1e8e99a20bab873cd3ac133215b940
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-09-01 15:44:05 +0530

    new program

---
+++ Csharp_day2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_day2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "najim";
+            char[] charArray = str.ToCharArray();
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                charArray[i] = str[j];
+                charArray[j] = str[i];
+            }
+            string reversedstring = new string(charArray);
+            Console.WriteLine(reversedstring);
+
+            Console.ReadLine();
+        }
+    }
+}
